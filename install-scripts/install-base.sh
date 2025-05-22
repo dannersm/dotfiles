634f499a37bb7ef3d2fffd136de54bd0a08a448f
Danner Schlotterbeck <danner.schmen@gmail.com>
2025-05-22 00:37:28 -0400

    move additional scripts to their corresponding parts and remove main install cause it didn't work anyway

--- install-scripts/install-base.sh
+++ install-scripts/install-base.sh
@@ -3,4 +3,9 @@ sudo apt install -y git curl wget tmux ripgrep fd-find neofetch unzip stow xclip
 
 # Install starship
 sudo curl -sS https://starship.rs/install.sh | sh
+echo 'eval "$(starship init bash)"' >> ~/.bashrc
+
+# stow configs
+stow -t ~/.config .config
+chmod +x ~/.config/polybar/system-bluetooth.sh
 
--- install-scripts/nvim/install-neovim.sh
+++ install-scripts/nvim/install-neovim.sh
@@ -5,9 +5,12 @@ curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x8
 chmod u+x nvim-linux-x86_64.appimage
 sudo mkdir -p /opt/nvim
 sudo mv nvim-linux-x86_64.appimage /opt/nvim/nvim
+echo 'export PATH="$PATH:/opt/nvim/"' >> ~/.bashrc
+echo "alias vim='nvim'" >> ~/.bashrc
 
 # clone config
 curr_dir=$(dirname $0)
 cd $curr_dir
 cd ../../.config
 git clone git@github.com:dannersm/nvim.git
+
