a6200ba1ada2beeec01a8f75a509a3254dca4246
Danner schlotterbeck <danner.schmen@gmail.com>
2025-05-21 23:25:52 -0400

    separate alacritty from rust

--- install-scripts/install-rust.sh
+++ install-scripts/install-rust.sh
@@ -3,5 +3,4 @@ source ~/.bashrc
 rustup override set stable
 rustup update stable
 sudo apt install cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev
-source ~/.bashrc
-cargo install alacritty
+
--- install.sh
+++ install.sh
@@ -4,7 +4,8 @@ sh install-scripts/install-base.sh
 sh install-scripts/install-font.sh
 sh install-scripts/install-python.sh
 sh install-scripts/install-node.sh
-sh install-scripts/install-alacritty.sh
+sh install-scripts/install-cargo.sh
+cargo install alacritty
 sh install-scripts/nvim/install-vectorcode.sh
 sh install-scripts/nvim/install-neovim.sh
 sh install-scripts/install-de.sh
