5d6bca73c95ae59b3b625475ebb32763a6ff4d90
Danner Schlotterbeck <danner.schmen@gmail.com>
2025-06-07 00:46:42 -0400

    fix tmux

--- install-scripts/install-base.sh
+++ install-scripts/install-base.sh
@@ -5,6 +5,10 @@ sudo apt install -y git curl wget tmux ripgrep fd-find neofetch unzip stow xclip
 sudo curl -sS https://starship.rs/install.sh | sh
 echo 'eval "$(starship init bash)"' >> ~/.bashrc
 
+# install tmux things
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+git clone -b v2.1.3 https://github.com/catppuccin/tmux.git ~/.config/tmux/plugins/catppuccin/tmux
+
 # stow configs
 stow -t ~/.config .config
 chmod +x ~/.config/polybar/system-bluetooth.sh
