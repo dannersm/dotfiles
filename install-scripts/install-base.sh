775a4a11bd6ab71d5bc75c46cce7d4c0e7e0f2a7
Danner Schlotterbeck <danner.schmen@gmail.com>
2025-06-10 02:23:31 -0400

    make separate chroma isntall script and a service file for the server

--- install-scripts/install-base.sh
+++ install-scripts/install-base.sh
@@ -4,6 +4,8 @@ sudo apt install -y git curl wget tmux ripgrep fd-find neofetch unzip stow xclip
 # Install starship
 sudo curl -sS https://starship.rs/install.sh | sh
 echo 'eval "$(starship init bash)"' >> ~/.bashrc
+echo "alias docs='source ~/.config/scripts/fzf_tmux_session.sh'" >> ~/.bashrc
+echo "alias spv='source ./.venv/bin/activate'" >> ~/.bashrc
 
 # install tmux things
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
@@ -11,5 +13,7 @@ git clone -b v2.1.3 https://github.com/catppuccin/tmux.git ~/.config/tmux/plugin
 
 # stow configs
 stow -t ~/.config .config
+sudo stow -t /etc/systemd/system service-files
+
 chmod +x ~/.config/polybar/system-bluetooth.sh
 
---
+++ install-scripts/install-chromadb.sh
@@ -0,0 +1,8 @@
+pipx install chromadb
+sudo mkdir /chroma/data
+sudo systemctl daemon-reload
+sudo systemctl daemon-reload
+sudo systemctl enable chroma
+sudo systemctl start chroma
+sudo systemctl enable chroma
+sudo systemctl start chroma
